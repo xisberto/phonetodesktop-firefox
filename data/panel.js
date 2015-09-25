052c7e1d86ecc138373e203b19d3a8a90a8f1725
Humberto Fraga <xisberto@gmail.com>
2015-09-25 18:28:37 -0300

    Authorization working

--- data/panel.js
+++ data/panel.js
@@ -1,8 +1,29 @@
 var btnStartAuth = document.getElementById("start_auth");
-btnStartAuth.addEventListener("click", checkAuth);
+btnStartAuth.addEventListener("click", function(event) {
+    self.port.emit("check-auth");    
+});
+var btnLogOut = document.getElementById("logout");
+btnLogOut.addEventListener("click", function(event) {
+    self.port.emit("logout");
+});
 
-function checkAuth(data) {
-    console.log("loading...");
-    self.port.emit("check-auth");
-}
 
+self.port.on("panel-opened", function(authenticated) {
+    console.log("authenticated: " + authenticated);
+    if (authenticated) {
+        document.getElementById("div_app").style.display = 'block';
+        document.getElementById("div_auth").style.display = 'none';
+    } else {
+        document.getElementById("div_app").style.display = 'none';
+        document.getElementById("div_auth").style.display = 'block';
+    }
+})
+
+self.port.on("load-list", function(tasks) {
+    var list = document.getElementById('list_tasks');
+    for (i in tasks) {
+        var item = document.createElement('li');
+        item.innerHTML = tasks[i].title;
+        list.appendChild(item);
+    }
+});
--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var { ToggleButton } = require('sdk/ui/button/toggle');
 var panels = require('sdk/panel');
 var self = require('sdk/self');
 var ss = require('sdk/simple-storage');
-var oauth = require('addon-google-oauth2');
+var request = require('sdk/request');
+var oauth = require('addon-google-oauth2')
 
 console.log("init");
+
+//Preparing UI components
 
 var button = ToggleButton({
     id: "ptd-button",
@@ -18,18 +21,20 @@ var button = ToggleButton({
 });
 
 var panel = panels.Panel({
-    contentURL: "./panel.html",
+    height: 480,
+    width: 320,
+    contentURL: "./popup.html",
     contentScriptFile: "./panel.js",
     onHide: onPanelHide
 });
-
 
 function showPanel(state) {
     if (state.checked) {
         panel.show({
             position: button
         });
-        console.log("Teste");
+        console.log("Panel opened");
+        getTasks();
     }
 }
 
@@ -38,21 +43,73 @@ function onPanelHide() {
 }
 
 function checkAuth() {
-    console.log("checking auth")
+    console.log("checking auth");
     var oauth2_options = {
-        client_id: "908808215735.apps.googleusercontent.com",
-        client_secret: "4rs0dvjj6PxW6F6C1dlvXP3y",
+        client_id: "396885376537-avfrd05q028n6bo2fpi6p26sarqo4822.apps.googleusercontent.com",
+        client_secret: "1WiaiN_m_D-PsaSf8Dyz8_Tu",
         scopes: "https://www.googleapis.com/auth/tasks"
     }
-    oauth.refreshToken(oauth2_options, callback);
+    oauth.refreshToken(oauth2_options, onAuthChecked);
 }
 
-function callback(token) {
-    console.log("callback called with token: " + token);
-    ss.storage.token = token;
+function onAuthChecked(token) {
+    console.log("onAuthChecked called with token: " + token);
+    var list_id = ss.storage.list_id;
+    if (list_id) {
+        getTasks();
+    } else {
+        saveListId(token, onAuthChecked);
+    }
+}
+
+function saveListId(access_token) {
+    console.log("saveListId called with token: " + access_token);
+    request.Request({
+        url: "https://www.googleapis.com/tasks/v1/users/@me/lists",
+        headers: {
+            "Authorization": "Bearer " + access_token
+        },
+        onComplete: function(resp) {
+            lists = resp.json.items;
+            for (i in lists) {
+                if (lists[i].title == "PhoneToDesktop") {
+                    console.log("saving list id");
+                    ss.storage.list_id = lists[i].id;
+                    getTasks();
+                    break;
+                }
+            }
+        }
+    }).get();
+}
+
+function getTasks() {
+    var token = ss.storage.access_token;
+    var list_id = ss.storage.list_id;
+    if (token == undefined || list_id == undefined) {
+        panel.port.emit("panel-opened", false);
+    } else {
+        panel.port.emit("panel-opened", true);
+        request.Request({
+            url: "https://www.googleapis.com/tasks/v1/lists/"+list_id+"/tasks",
+            headers: {
+                "Authorization": "Bearer " + token
+            },
+            onComplete: function(resp) {
+                panel.port.emit("load-list", resp.json.items);
+            }
+        }).get();    
+    }
 }
 
 panel.port.on("check-auth", function (){
     panel.hide();
     checkAuth();
+});
+
+panel.port.on("logout", function(){
+    delete ss.storage.list_id;
+    delete ss.storage.access_token;
+    delete ss.storage.refresh_token;
+    panel.port.emit("panel-opened", false);
 });
